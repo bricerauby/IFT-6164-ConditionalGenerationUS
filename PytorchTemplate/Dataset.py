6ca50aeec0225346896764307eea9234a66e9775
J-bytes <60334318+J-bytes@users.noreply.github.com>
2023-02-28 18:08:59 -0500

    Initial commit

---
+++ PytorchTemplate/Dataset.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-02-24$
+
+@author: Jonathan Beaulieu-Emond
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-02-20$
+
+@author: Jonathan Beaulieu-Emond
+"""
+
+import torch
+import numpy as np
+from torchvision import transforms
+from torchvision import datasets
+from PytorchTemplate.augmentations import  CutMix,MixUp,CutOut
+import inspect
+
+#@torch.jit.script
+
+
+
+train_transform=transforms.Compose(
+        [
+
+            transforms.TrivialAugmentWide(interpolation=transforms.InterpolationMode.BILINEAR),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+
+            transforms.RandomErasing(p=0.3)
+
+        ]
+    )
+
+def Dataset(Dataset : torch.utils.data.Dataset or str,num_classes : int,batch_size,valid_size,shuffle,num_workers,pin_memory,mean,std,root,debug,prob,label_smoothing,extras={}) :
+    """
+
+    :param dataset: Pytorch Dataset or string of a dataset name in torchvision.datasets
+    :param num_classes: The number of classes in the dataset
+    :param batch_size: The batch size. If 0 , We will try to guess the maximum batch size possible for the GPU
+    :param valid_size: The ratio of image to use for validation. Suggested value : 0.1
+    :param shuffle: Should the training dataset be shuffled?
+    :param num_workers: The number of process to create to load the data. On windows, this value needs to be set to 0
+    :param pin_memory: Leave it to True
+    :param mean: The mean accross channel for the dataset
+    :param std: The standard deviation accross channel for the dataset
+    :param root: The location of the dataset / where to download the dataset
+    :param debug: If true, set the number of samples per epoch to 1000 in order to debug the code
+    :param prob: Probability for each advanced augmentation to be applied [P_CutMix,P_MixUp,P_CutOut]
+    :param label_smoothing: Value of label smoothing . Suggested value : 0-0.1
+    :param extras: Extra parameter needed to initialize the dataset in a dictionnary
+    :return: train_loader,valid_loader
+    """
+
+
+    #-------- Normalization ----------------
+    normalize = transforms.Normalize(mean, std)
+    def normalization(images):
+
+        for ex, image in enumerate(images):
+            images[ex] = normalize(image)
+
+        return images
+
+    #-------- target transformation ----------------
+
+    class target_transform:
+        def __init__(self, smooth: float = 0, num_classes: int = 10):
+            self.smooth = smooth
+            self.num_classes = num_classes
+        def __call__(self, i: int):
+            label = torch.zeros((self.num_classes,)) + self.smooth
+            label[i] = 1 - self.smooth
+
+            return label
+
+
+    #-------- Advanced Transform ----------------
+    advanced_transform = transforms.Compose([
+        CutMix.CutMix(p=prob[0], intensity=0.4),
+        MixUp.MixUp(p=prob[1], intensity=0.4),
+        # CutOut.CutOut(p=prob[2], intensity=0.4), -> already implemented in trivial augment wide
+    ])
+
+
+    #-------- Dataset ----------------
+    if type(Dataset) == str :
+        assert Dataset in dir (datasets), "Dataset not found in torchvision.datasets"
+        Dataset = getattr(datasets,Dataset)
+
+    signature = inspect.signature(Dataset.__init__)
+
+    cfg = {
+        "root" : root,
+        "train" : True,
+        "download" : True,
+        "transform" : train_transform,
+        "target_transform" : target_transform(smooth=label_smoothing, num_classes=num_classes)
+    } | extras
+
+    cfg = {k:v for k,v in cfg.items() if k in signature.parameters}
+    dataset = Dataset(**cfg)
+    setattr(dataset, "advanced_transform", advanced_transform)
+    setattr(dataset, "normalize", normalization)
+
+
+    num_train = len(dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        # np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+
+    # if debug :
+    #     train_idx, valid_idx,test_idx = indices[0:10], indices[10:20],indices[20:30]
+    #     train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_idx)
+    #     valid_sampler = torch.utils.data.sampler.SubsetRandomSampler(valid_idx)
+    #     test_sampler = torch.utils.data.sampler.SubsetRandomSampler(test_idx)
+    #     test_loader = torch.utils.data.DataLoader(
+    #         training_data, batch_size=batch_size, sampler=test_sampler,
+    #         num_workers=num_workers, pin_memory=pin_memory,
+    #     )
+    # else :
+
+    if "train" in signature.parameters.keys() :
+        cfg["train"]=False
+        test_data = Dataset(**cfg)
+
+        setattr(test_data, "normalize", normalization)
+        split = int(np.floor(valid_size * num_train))
+        if debug :
+            train_idx, valid_idx = indices[0:100], indices[100:200]
+        else :
+            train_idx, valid_idx = indices[split:], indices[:split]
+        train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_idx)
+        valid_sampler = torch.utils.data.sampler.SubsetRandomSampler(valid_idx)
+        test_loader = torch.utils.data.DataLoader(
+            test_data, batch_size=batch_size, shuffle=False,
+            num_workers=num_workers, pin_memory=pin_memory,
+        )
+    else :
+        split = int(np.floor(valid_size * 2 * num_train))
+        train_idx, valid_idx,test_idx = indices[0:-split],indices[-split : -split//2 ],indices[-split//2 : ]
+        train_sampler = torch.utils.data.sampler.SubsetRandomSampler(train_idx)
+        valid_sampler = torch.utils.data.sampler.SubsetRandomSampler(valid_idx)
+        test_sampler = torch.utils.data.sampler.SubsetRandomSampler(test_idx)
+
+
+        test_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, sampler=test_sampler,
+            num_workers=num_workers, pin_memory=pin_memory,
+        )
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, sampler=train_sampler,
+        num_workers=num_workers, pin_memory=pin_memory,
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, sampler=valid_sampler,
+        num_workers=num_workers, pin_memory=pin_memory,
+    )
+
+    return train_loader,valid_loader,test_loader
+
+
+#@torch.jit.script
+class normalization :
+    def __init__(self,mean : torch.Tensor,std : torch.Tensor,mode : str="train") :
+
+        self.mode = mode
+        self.normalize = transforms.Normalize(mean,std)
+
+
+
+
+
+    def __call__(self,images):
+
+        for ex,image in enumerate(images) :
+            images[ex] = self.normalize(image)
+
+        return images
+
+
+
+
+
+if __name__ == "__main__":
+    train_loader,val_loader,test_loader = Dataset(Dataset = "CIFAR10",num_classes=10,batch_size=32, valid_size=0.1, shuffle=True, num_workers=0, pin_memory=False, mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5), root="../../data",debug=False,prob=[1,1],label_smoothing=0.1)
+
+    image,label = next(iter(train_loader))
+
+    training_data = datasets.CIFAR10
+
+    train_loader, val_loader,test_loader = Dataset(Dataset=training_data, num_classes=10, batch_size=32, valid_size=0.1, shuffle=True,
+                                       num_workers=0, pin_memory=False, mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5),
+                                       root="../../data", debug=False, prob=[1, 1], label_smoothing=0.1,extras = {"something" : 1})
---
+++ PytorchTemplate/Metrics.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+import timm.utils.metrics
+
+from sklearn.metrics import f1_score,recall_score,precision_score,roc_auc_score
+
+
+from PytorchTemplate import names
+
+class Metrics:
+    def __init__(self):
+        self.num_classes = len(names)
+        self.thresholds = [0.5,]*self.num_classes
+        self.names = names
+
+    # def convert(self,pred):
+    #
+    #     for i in range(self.num_classes) :
+    #         pred[:,i] = np.where(
+    #             pred[:,i]<=self.thresholds[i],
+    #             pred[:,i]/2/self.thresholds[i],               #if true
+    #             1 - (1-pred[:,i])/2/(1-self.thresholds[i])    #if false
+    #         )
+    #     return pred
+
+
+        self.convert = lambda x : x #if you wish to use default threshold
+
+
+
+    def accuracy3(self, true, pred):
+        # n, m = true.shape
+        # pred2 = self.convert(pred)
+        # pred2 = np.where(pred2 > 0.5, 1, 0)
+        #
+        # accuracy = 0
+        # for x, y in zip(true, pred2):
+        #     if (x == y).all():
+        #         accuracy += 1
+        # accuracy /=n
+        accuracy = timm.utils.metrics.accuracy(pred,true, topk=(3,))
+        return accuracy
+    def accuracy(self, true, pred):
+
+        true = np.argmax(true,axis=1)
+        pred = np.argmax(pred,axis=1)
+        accuracy = np.sum(true==pred)/len(true)
+
+        return accuracy
+
+    def f1(self, true, pred):
+
+        pred2 = self.convert(pred)
+
+        pred2 = np.where(pred2 > 0.5, 1, 0)
+
+
+        return f1_score(
+            true, pred2, zero_division=0,average="macro"
+        )  # weighted??
+
+    def precision(self, true, pred):
+        pred = self.convert(pred)
+        pred = np.where(pred > 0.5, 1, 0)
+        results = precision_score(true, pred, average=None, zero_division=0)
+
+        results_dict = {}
+        for item, name in zip(results, self.names):
+            results_dict[name] = item
+        return results_dict
+
+    def recall(self, true, pred):
+
+        pred = self.convert(pred)
+        pred = np.where(pred > 0.5, 1, 0)
+        results=recall_score(true, pred, average=None, zero_division=0)
+        results_dict={}
+        for item,name in zip(results,self.names) :
+            results_dict[name] = item
+        return results_dict
+
+    def computeAUROC_weighted(self, true, pred):
+        fpr = dict()
+        tpr = dict()
+        outAUROC = dict()
+        classCount = pred.shape[1]
+        for i in range(classCount):
+
+            # fpr[i], tpr[i], thresholds = roc_curve(true[:, i], pred[:, i],pos_label=1)
+            #
+            # threshold = thresholds[np.argmax(tpr[i] - fpr[i])]
+            # logging.info(f"threshold {self.names[i]} : ",threshold)
+            # self.thresholds[i] =threshold
+            # try :
+            #     auroc =  auc(fpr[i], tpr[i])
+            # except :
+            #     auroc=0
+            try :
+                auroc = roc_auc_score(true[:, i], pred[:, i],average="weighted")
+            except ValueError:
+                auroc = 0
+            outAUROC[self.names[i]] = auroc
+            if np.isnan(outAUROC[self.names[i]]):
+                outAUROC[self.names[i]] = 0
+
+        outAUROC["mean"] = np.mean(list(outAUROC.values()))
+
+
+        return outAUROC
+
+    def computeAUROC(self, true, pred):
+        fpr = dict()
+        tpr = dict()
+        outAUROC = dict()
+        classCount = pred.shape[1]
+        for i in range(classCount):
+
+            # fpr[i], tpr[i], thresholds = roc_curve(true[:, i], pred[:, i],pos_label=1)
+            #
+            # threshold = thresholds[np.argmax(tpr[i] - fpr[i])]
+            # logging.info(f"threshold {self.names[i]} : ",threshold)
+            # self.thresholds[i] =threshold
+            # try :
+            #     auroc =  auc(fpr[i], tpr[i])
+            # except :
+            #     auroc=0
+            try :
+                auroc = roc_auc_score(true[:, i], pred[:, i],average="weighted")
+            except ValueError:
+                auroc = 0
+            outAUROC[self.names[i]] = auroc
+            if np.isnan(outAUROC[self.names[i]]):
+                outAUROC[self.names[i]] = 0
+
+        outAUROC["mean"] = np.mean(list(outAUROC.values()))
+
+
+        return outAUROC
+
+    def metrics(self):
+        dict = {
+            "accuracy": self.accuracy,
+
+        }
+        return dict
+
+
+metrics = Metrics().metrics()
+
+if __name__=="__main__" :
+    from PytorchTemplate import names
+    num_classes=len(names)
+    metric = Metrics()
+    metrics = metric.metrics()
+    print(metrics)
+    label=np.random.randint(0,2,(10,num_classes))
+    pred =np.random.random(size=(10,num_classes))
+    for key,metric in metrics.items() :
+        metric(label,pred)
