952df557219744338c731a54d69c2546399956bf
Brice Rauby <brice.rauby@polymtl.ca>
2023-04-16 11:51:18 -0700

    small dataset refacto

--- dataset/ClassifierDataset.py
+++ dataset/ClassifierDataset.py
@@ -2,32 +2,10 @@ import os
 import torch
 import h5py
 import numpy as np
-from torchvision import transforms
 
-
-class ClassifierDataset(torch.utils.data.Dataset):
-    def __init__(self, prefix, pathMb, pathNoMb, num_frames=None):
-        # Store the path for the HDF5 file
-        self.MBdataPath = os.path.join(prefix, pathMb)
-        self.noMBdataPath = os.path.join(prefix, pathNoMb)
-        # Create an empty list to store data information
-        self.data_info = []
-        for path, label in zip([self.noMBdataPath, self.MBdataPath], [0, 1]):
-            # Open the HDF5 file and retrieve the 'patches' dataset
-            # keeping in mind that Matlab 
-            with h5py.File(path, 'r') as h5f:
-                patches_dataset = h5f['patches']
-
-                # Get the total number of patches and the number of frames per patch
-                num_patches = patches_dataset.shape[-1]
-                if num_frames is None:
-                    num_frames = patches_dataset.shape[0]
-
-                # Iterate through each patch index and frame index
-                for patch_idx in range(num_patches):
-                    for frame_idx in range(num_frames):
-                        self.data_info.append((path, patch_idx, frame_idx, label))
-
+class PatchDataset(torch.utils.data.Dataset):
+    def __init__(self, data_info):
+        self.data_info = data_info
     def __len__(self):
         # Return the total number of samples in the dataset
         return len(self.data_info)
@@ -48,6 +26,30 @@ class ClassifierDataset(torch.utils.data.Dataset):
         # Return the frame tensor
 
         return frame_tensor, label
+
+class ClassifierDataset(PatchDataset):
+    def __init__(self, prefix, pathMb, pathNoMb, num_frames=None):
+        # Store the path for the HDF5 file
+        MBdataPath = os.path.join(prefix, pathMb)
+        noMBdataPath = os.path.join(prefix, pathNoMb)
+        # Create an empty list to store data information
+        data_info = []
+        for path, label in zip([noMBdataPath, MBdataPath], [0, 1]):
+            # Open the HDF5 file and retrieve the 'patches' dataset
+            # keeping in mind that Matlab 
+            with h5py.File(path, 'r') as h5f:
+                patches_dataset = h5f['patches']
+
+                # Get the total number of patches and the number of frames per patch
+                num_patches = patches_dataset.shape[-1]
+                if num_frames is None:
+                    num_frames = patches_dataset.shape[0]
+
+                # Iterate through each patch index and frame index
+                for patch_idx in range(num_patches):
+                    for frame_idx in range(num_frames):
+                        data_info.append((path, patch_idx, frame_idx, label))
+        super().__init__(data_info)
 
 
 
